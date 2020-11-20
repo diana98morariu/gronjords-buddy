8f7acd945989a66137c813152cbe032cadd64896
decka <steriuandreea@yahoo.ro>
2020-11-20 11:53:51 +0100

    basic frontend react

---
+++ frontend/src/app/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import classes from "./App.module.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/Header/Header.js";
+import Home from "./pages/Login/Login.js";
+
+const App = () => {
+  return (
+    <div className={classes.App}>
+      <Router basename="/">
+        <Header />
+        <div className={classes.Root}>
+          <Switch>
+            <Route exact path="/" component={(props) => <Home {...props} />} />
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
