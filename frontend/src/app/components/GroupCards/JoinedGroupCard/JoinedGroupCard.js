c5886037f41ce10bf81f18926e8a0b8018989fb7
Diana Morariu <32464376+diana98morariu@users.noreply.github.com>
2021-01-16 18:59:10 +0100

    changes, groups page

--- frontend/src/app/components/GroupCards/JoinedGroupCard/JoinedGroupCard.js
+++ frontend/src/app/components/GroupCards/JoinedGroupCard/JoinedGroupCard.js
@@ -2,11 +2,17 @@ import React, { useState, useEffect } from "react";
 import classes from "./JoinedGroupCard.module.css";
 
 const JoinedGroupCard = (props) => {
-  const { group_name } = props.joinedGroup;
+  const { group_name, image } = props.joinedGroup;
 
   return (
     <div className={classes.JoinedGroupContainer}>
-      <div className={classes.JoinedGroupImage}></div>
+      <div className={classes.JoinedGroupImage}>
+        <img
+          src={"https://gronjords-buddy.s3.eu-north-1.amazonaws.com/" + image}
+          className={classes.GroupImage}
+          alt={image}
+        />
+      </div>
       <div className={classes.JoinedGroupName}>{group_name}</div>
     </div>
   );
--- frontend/src/app/components/GroupCards/NotJoinedGroupCard/NotJoinedGroupCard.js
+++ frontend/src/app/components/GroupCards/NotJoinedGroupCard/NotJoinedGroupCard.js
@@ -1,10 +1,45 @@
 import React, { useState, useEffect } from "react";
 import classes from "./NotJoinedGroupCard.module.css";
+import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+
+const JoinButton = withStyles({
+  root: {
+    width: "100%",
+    height: "30px",
+    fontSize: "14px",
+    backgroundColor: "#d3d3d3",
+    boxShadow: "none",
+    "&:hover": {
+      boxShadow: "none",
+      backgroundColor: "rgba(0,0,0,0)",
+    },
+    textTransform: "none",
+  },
+})(Button);
 
 const NotJoinedGroupCard = (props) => {
-  const { group_name } = props.notJoinedGroup;
+  const { group_name, image } = props.notJoinedGroup;
 
-  return <div className={classes.NotJoinedGroupContainer}>{group_name}</div>;
+  return (
+    <React.Fragment>
+      <div className={classes.NotJoinedGroupContainer}>
+        <div className={classes.NotJoinedGroupImage}>
+          <img
+            src={"https://gronjords-buddy.s3.eu-north-1.amazonaws.com/" + image}
+            className={classes.GroupImage}
+            alt={image}
+          />
+        </div>
+        <div className={classes.NotJoinedGroupName}>{group_name}</div>
+        <div className={classes.JoinButton}>
+          <JoinButton variant="contained" size="small">
+            Join
+          </JoinButton>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default NotJoinedGroupCard;
