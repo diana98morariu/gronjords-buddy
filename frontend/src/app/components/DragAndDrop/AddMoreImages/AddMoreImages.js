15eb54f9dd2262dd5c70e2298b4693013254f36d
Diana Morariu <32464376+diana98morariu@users.noreply.github.com>
2021-01-17 22:37:39 +0100

    drag and drop

---
+++ frontend/src/app/components/DragAndDrop/AddMoreImages/AddMoreImages.js
@@ -0,0 +1,18 @@
+import React from "react";
+import classes from "./AddMoreImages.module.css";
+
+const AddMoreImages = (props) => {
+  return (
+    <span className={classes.AddMoreContainer}>
+      <input
+        className={classes.HiddenInputFile}
+        type="file"
+        multiple="multiple"
+        accept="image/*"
+        onChange={(e) => props.addImages(e)}
+      />
+    </span>
+  );
+};
+
+export default AddMoreImages;
